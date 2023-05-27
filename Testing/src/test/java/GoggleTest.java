769c55854eb308f34e287a80664834d24ac15876
julius-kom <pir-pir-piiir@mail.ru>
2023-05-27 19:04:08 +0300

    wait.until

--- Testing/src/test/java/GoggleTest.java
+++ Testing/src/test/java/GoggleTest.java
@@ -2,31 +2,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
 public class GoggleTest {
     private WebDriver driver;
-    //private WebDriverWait wait;
+    private WebDriverWait wait;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
-        //wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
     }
 
     @Test
     public void newSearch() {
         driver.get("https://google.com");
-        driver.findElement(By.name("q")).sendKeys("самый простой тестовый запрос\n");
-        //driver.findElement(By.name("btnK")).;
-        //wait.until(titleIs("самый простой тестовый запрос - Поиск в Google"));
-        //element(byName("q")).setValue("самый простой тестовый запрос").pressEnter();
+        driver.findElement(By.name("q")).sendKeys("selenium" + Keys.ENTER);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a/h3")));
     }
 
     @After
